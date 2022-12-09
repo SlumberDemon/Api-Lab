c50bc9c7908b5240fad0e14476487ae321cb6ce5
Sofa <Nextil3o3@gmail.com>
2022-12-09 16:09:55 +0100

    api fetches

---
+++ static/scripts/script.js
@@ -0,0 +1,13 @@
+let SendButton = document.getElementById("api-send");
+let OutputField = document.getElementById("output");
+let ApiType = document.getElementById("api-type");
+let ApiUrl = document.getElementById("api-url");
+
+
+SendButton.addEventListener("click", async () => {
+    OutputField.innerHTML = `Fetching...`
+    let rsp = await fetch(`${ApiUrl.value}`, { method: ApiType.value}) 
+    let data = await rsp.json()
+    console.log(data)
+    OutputField.innerHTML = data
+})
